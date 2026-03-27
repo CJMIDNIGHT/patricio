4b8558f2c9b9d889bd29620c5bc971b7d45e8cba
Adenor <amburet@epsg.upv.es>
2026-03-27 15:32:17 +0100

    inserción del robot correcto y funcional con pruebas de funcionamiento de los sensores

--- patricio_my_world/launch/house.launch.py
+++ patricio_my_world/launch/house.launch.py
@@ -1,21 +1,69 @@
 import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration
-from launch_ros.actions import Node
-from launch.actions import IncludeLaunchDescription
+from launch.actions import AppendEnvironmentVariable, IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+
 
 def generate_launch_description():
-    pkg = get_package_share_directory('patricio_my_world')
+    pkg        = get_package_share_directory('patricio_my_world')
+    ros_gz_sim = get_package_share_directory('ros_gz_sim')
+    launch_dir = os.path.join(pkg, 'launch')
+
+    use_sim_time = LaunchConfiguration('use_sim_time', default='true')
+    x_pose       = LaunchConfiguration('x_pose', default='0.0')
+    y_pose       = LaunchConfiguration('y_pose', default='-1.0')
+
     world = os.path.join(pkg, 'worlds', 'house.sdf')
 
-    gz_sim = IncludeLaunchDescription(
+    gzserver_cmd = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            os.path.join(get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')
+            os.path.join(ros_gz_sim, 'launch', 'gz_sim.launch.py')
         ),
-        launch_arguments={'gz_args': world}.items()
+        launch_arguments={
+            'gz_args': ['-r -s -v2 ', world],
+            'on_exit_shutdown': 'true'
+        }.items()
     )
 
-    return LaunchDescription([gz_sim])
+    gzclient_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(ros_gz_sim, 'launch', 'gz_sim.launch.py')
+        ),
+        launch_arguments={
+            'gz_args': '-g -v2 ',
+            'on_exit_shutdown': 'true'
+        }.items()
+    )
+
+    robot_state_publisher_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(launch_dir, 'robot_state_publisher.launch.py')
+        ),
+        launch_arguments={'use_sim_time': use_sim_time}.items()
+    )
+
+    spawn_turtlebot_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(launch_dir, 'spawn_turtlebot3.launch.py')
+        ),
+        launch_arguments={
+            'x_pose': x_pose,
+            'y_pose': y_pose
+        }.items()
+    )
+
+    set_env_vars_resources = AppendEnvironmentVariable(
+        'GZ_SIM_RESOURCE_PATH',
+        os.path.join(pkg, 'models')
+    )
+
+    ld = LaunchDescription()
+    ld.add_action(set_env_vars_resources)
+    ld.add_action(gzserver_cmd)
+    ld.add_action(gzclient_cmd)
+    ld.add_action(robot_state_publisher_cmd)
+    ld.add_action(spawn_turtlebot_cmd)
+
+    return ld
